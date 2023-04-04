885ee7d178730f50bc63acd592d163974d505683
Iryna Fedorova <i.fedorova4321@gmailcom>
2023-04-04 21:40:25 +0200

    Homework 31

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './css/App.css';
+import Header from './Header.js'
+import Main from './Main.js'
+
+export default () => {
+    return <div className='container'>
+    <Header />
+    <Main />
+    </div> 
+}
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import './css/Header.css';
+
+export default () => {
+    return <div className="menuContainer">
+        <div className="menuItem">
+            <a href='#'>Головна</a>
+        </div>
+        <div className="menuItem">
+            <a href='#'>Питання</a>
+        </div>
+        <div className="menuItem">
+            <a href='#'>Матеріали</a>
+        </div>
+        <div className="menuItem">
+            <a href='#'>Контакти</a>
+        </div>
+    </div>
+}
